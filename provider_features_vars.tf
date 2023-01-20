aafc6df8d42374d8b8e5a1cad610e4f2b9efcf17
vladimir.dudnic-ext <vladimir.dudnic-ext@alstomgroup.com>
2023-01-20 09:38:34 +0100

    update provider_features_vars.tf

--- provider_features_vars.tf
+++ provider_features_vars.tf
@@ -1,21 +1,21 @@
   
   variable "subscription_id" {
     type = string
-    default = "2c7684a0-ec26-4999-bce8-40fadd9d7b3f"
+    
   }
 
   variable "client_id" {
     type = string
-    default = "b9770af7-0c56-463a-a6c3-012a90d6f0bd"
+    
   }
 
   variable "client_certificate_path" {
     type = string
-    default = "./service-principal.pfx"
+    
   }
   
   variable "tenant_id" {
     type = string
-    default = "029c7c0d-55be-409d-9798-41f3f7837048"
+    
   }
   
